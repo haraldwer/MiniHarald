e9199327d0995f76bc2cf6d62b2f3ce722a1ce28
Harald Wergeland <harwer009@gmail.com>
2025-12-11 18:53:39 +0100

    Stuff

--- Project/Character.cs
+++ Project/Character.cs
@@ -5,151 +5,151 @@ using Godot;
 
 public partial class Character : Node2D
 {
-    [Export] public Label Label;
-    [Export] public TextWindow TextWindow;
-    [Export] public Window[] GiftWindows;
-    [Export] public Movement Movement;
-    [Export] public Talking Talking;
-    [Export] public BehaviorManager Behavior;
-    [Export] public EyeAnimator Eyes;
-    [Export] public MouthAnimator Mouth;
-    [Export] public WalkAnimator Walk;
-    [Export] public HandAnimator Hands;
-    [Export] public Sprite2D Shirt;
-    [Export] public Sprite2D Hat;
-    
-    private static Character? Instance;
-    public RandomNumberGenerator Rnd = new();
-    
-    Character()
-    {
-        Instance = this;
-    }
+	[Export] public Label Label;
+	[Export] public TextWindow TextWindow;
+	[Export] public Window[] GiftWindows;
+	[Export] public Movement Movement;
+	[Export] public Talking Talking;
+	[Export] public BehaviorManager Behavior;
+	[Export] public EyeAnimator Eyes;
+	[Export] public MouthAnimator Mouth;
+	[Export] public WalkAnimator Walk;
+	[Export] public HandAnimator Hands;
+	[Export] public Sprite2D Shirt;
+	[Export] public Sprite2D Hat;
+	
+	private static Character? Instance;
+	public RandomNumberGenerator Rnd = new();
+	
+	Character()
+	{
+		Instance = this;
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-        
-        //AutoGenMips();
+	public override void _Ready()
+	{
+		base._Ready();
+		
+		//AutoGenMips();
 
-        var c = Shirt.SelfModulate;
-        Shirt.SetSelfModulate(Color.FromHsv(Rnd.Randf(), c.S, c.V));
-        
-        GetWindow().FilesDropped += OnFilesDropped;
-    }
+		var c = Shirt.SelfModulate;
+		Shirt.SetSelfModulate(Color.FromHsv(Rnd.Randf(), c.S, c.V));
+		
+		GetWindow().FilesDropped += OnFilesDropped;
+	}
 
-    void AutoGenMips()
-    {
-        List<Node> children = GetChildren().ToList();
-        while (children.Count > 0)
-        {
-            List<Node> nextChildren = new();
-            foreach (var c in children)
-            {
-                nextChildren.AddRange(c.GetChildren());
-                if (c is Sprite2D sprite)
-                {
-                    sprite.TextureFilter = TextureFilterEnum.LinearWithMipmapsAnisotropic;
-                    var tex = sprite.GetTexture();
-                    if (tex != null)
-                    {
-                        var img = tex.GetImage();
-                        if (GenMip(img))
-                            sprite.SetTexture(ImageTexture.CreateFromImage(img));
-                    }
-                }
-            }
-            children = nextChildren;
-        }
-    }
+	void AutoGenMips()
+	{
+		List<Node> children = GetChildren().ToList();
+		while (children.Count > 0)
+		{
+			List<Node> nextChildren = new();
+			foreach (var c in children)
+			{
+				nextChildren.AddRange(c.GetChildren());
+				if (c is Sprite2D sprite)
+				{
+					sprite.TextureFilter = TextureFilterEnum.LinearWithMipmapsAnisotropic;
+					var tex = sprite.GetTexture();
+					if (tex != null)
+					{
+						var img = tex.GetImage();
+						if (GenMip(img))
+							sprite.SetTexture(ImageTexture.CreateFromImage(img));
+					}
+				}
+			}
+			children = nextChildren;
+		}
+	}
 
-    bool GenMip(Image InImage)
-    {
-        InImage.PremultiplyAlpha();
-        InImage.FixAlphaEdges();
-        if (InImage.HasMipmaps()) 
-            return true;
-        var err = InImage.GenerateMipmaps();
-        GD.Print("Mips generated: " + InImage.ResourcePath);
-        return err == Error.Ok;
-    }
+	bool GenMip(Image InImage)
+	{
+		InImage.PremultiplyAlpha();
+		InImage.FixAlphaEdges();
+		if (InImage.HasMipmaps()) 
+			return true;
+		var err = InImage.GenerateMipmaps();
+		GD.Print("Mips generated: " + InImage.ResourcePath);
+		return err == Error.Ok;
+	}
 
-    private void OnFilesDropped(string[] files)
-    {
-        if (files.IsEmpty())
-            return;
-        var image = Image.LoadFromFile(files.First());
-        GenMip(image);
-        var tex = ImageTexture.CreateFromImage(image);
-        Hat.Texture = tex;
-        Hat.Rotation = Mathf.DegToRad(Rnd.RandfRange(-15, 15));
-        Talking.Say("En mössa!");
-    }
+	private void OnFilesDropped(string[] files)
+	{
+		if (files.IsEmpty())
+			return;
+		var image = Image.LoadFromFile(files.First());
+		GenMip(image);
+		var tex = ImageTexture.CreateFromImage(image);
+		Hat.Texture = tex;
+		Hat.Rotation = Mathf.DegToRad(Rnd.RandfRange(-15, 15));
+		Talking.Say("En mössa!");
+	}
 
-    public static Character Get() => Instance;
-    
-    public static float ViewportScale()
-    {
-        if (Instance != null)
-            return Instance.GlobalScale.Length();
-        return 1.0f;
-    }
+	public static Character Get() => Instance;
+	
+	public static float ViewportScale()
+	{
+		if (Instance != null)
+			return Instance.GlobalScale.Length();
+		return 1.0f;
+	}
 
-    public static Vector2 ScreenToWorld(Vector2 InScreen)
-    {
-        return InScreen / ViewportScale();
-    }
-    
-    public static Vector2 WorldToChar(Vector2 InPos)
-    {
-        return InPos - Instance.Position / ViewportScale();
-    }
-    
-    public static Vector2 CharToWorld(Vector2 InPos)
-    {
-        return InPos * ViewportScale() + Instance.Position;
-    }
-    
-    public static Vector2 WorldToScreen(Vector2 InPos)
-    {
-        return InPos * ViewportScale();
-    }
-    
-    public static Vector2 MousePos()
-    {
-        if (Instance != null)
-            return WindowPos() + Instance.GetWindow().GetMousePosition();
-        return Vector2.Zero;
-    }
+	public static Vector2 ScreenToWorld(Vector2 InScreen)
+	{
+		return InScreen / ViewportScale();
+	}
+	
+	public static Vector2 WorldToChar(Vector2 InPos)
+	{
+		return InPos - Instance.Position / ViewportScale();
+	}
+	
+	public static Vector2 CharToWorld(Vector2 InPos)
+	{
+		return InPos * ViewportScale() + Instance.Position;
+	}
+	
+	public static Vector2 WorldToScreen(Vector2 InPos)
+	{
+		return InPos * ViewportScale();
+	}
+	
+	public static Vector2 MousePos()
+	{
+		if (Instance != null)
+			return WindowPos() + Instance.GetWindow().GetMousePosition();
+		return Vector2.Zero;
+	}
 
-    public static Vector2 WindowPos()
-    {
-        if (Instance != null)
-            return Instance.GetWindow().GetPosition();
-        return Vector2.Zero;
-    }
-    
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton mouseEvent)
-        {
-            if (mouseEvent.Pressed)
-            {
-                switch (mouseEvent.ButtonIndex)
-                {
-                    case MouseButton.Right:
-                        Behavior.Set(Behavior.Get<BehaviorWave>());
-                        break;
-                    case MouseButton.Left:
-                        Behavior.Set(Behavior.Get<BehaviorFly>());
-                        break;
-                }
-            }
-            else
-            {
-                if (Behavior.GetCurrent()?.GetType() == typeof(BehaviorFly))
-                    Behavior.Set(Behavior.Get<BehaviorLand>());
-            }
-        }
-    }
+	public static Vector2 WindowPos()
+	{
+		if (Instance != null)
+			return Instance.GetWindow().GetPosition();
+		return Vector2.Zero;
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (mouseEvent.Pressed)
+			{
+				switch (mouseEvent.ButtonIndex)
+				{
+					case MouseButton.Right:
+						Behavior.Set(Behavior.Get<BehaviorWave>());
+						break;
+					case MouseButton.Left:
+						Behavior.Set(Behavior.Get<BehaviorFly>());
+						break;
+				}
+			}
+			else
+			{
+				if (Behavior.GetCurrent()?.GetType() == typeof(BehaviorFly))
+					Behavior.Set(Behavior.Get<BehaviorLand>());
+			}
+		}
+	}
 }
--- Project/ProcessUtility.cs
+++ Project/ProcessUtility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,110 +10,112 @@ namespace NewGameProject;
 
 public class ProcessUtility
 {
-    static RandomNumberGenerator Rnd = new();
-    
-    [DllImport("user32.dll")]
-    public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+	static RandomNumberGenerator Rnd = new();
+	
+	[DllImport("user32.dll")]
+	public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
 
-    public struct Rect {
-        public int Left { get; set; }
-        public int Top { get; set; }
-        public int Right { get; set; }
-        public int Bottom { get; set; }
-    }
-    
-    static List<Process> LoadProcesses()
-    {
-        List<Process> result = new();
-        List<Process> processes = Process.GetProcesses().ToList();
-        processes.Sort((p1, p2) => p1.ProcessName.CompareTo(p2.ProcessName));
-        foreach (var p in processes)
-        {
-            try
-            {
-                if (p.BasePriority < 8 ||
-                    p.HasExited ||
-                    string.IsNullOrEmpty(p.MainWindowTitle))
-                    continue;
-                var t = p.StartTime; // Try access time
-            }
-            catch
-            {
-                continue;
-            }
-            if (result.Any((q) => p.ProcessName.Contains(q.ProcessName) || q.ProcessName.Contains(p.ProcessName)))
-                continue;
-            result.Add(p);
-        }
-        return result;
-    }
+	public struct Rect {
+		public int Left { get; set; }
+		public int Top { get; set; }
+		public int Right { get; set; }
+		public int Bottom { get; set; }
+	}
+	
+	static List<Process> LoadProcesses()
+	{
+		List<Process> result = new();
+		List<Process> processes = Process.GetProcesses().ToList();
+		processes.Sort((p1, p2) => p1.ProcessName.CompareTo(p2.ProcessName));
+		foreach (var p in processes)
+		{
+			try
+			{
+				if (p.BasePriority < 8 ||
+					p.HasExited ||
+					string.IsNullOrEmpty(p.MainWindowTitle))
+					continue;
+				var t = p.StartTime; // Try access time
+			}
+			catch
+			{
+				continue;
+			}
+			if (result.Any((q) => p.ProcessName.Contains(q.ProcessName) || q.ProcessName.Contains(p.ProcessName)))
+				continue;
+			result.Add(p);
+		}
+		return result;
+	}
 
-    public static bool HasProcess(string InName)
-    {
-        return LoadProcesses().Any((p) => p.ProcessName.Contains(InName));
-    }
-    
-    public static bool IsPlayingMusic()
-    {
-        NowPlayingSessionManager npsm = new NowPlayingSessionManager();
-        return npsm.Count > 0;
-    }
+	public static bool HasProcess(string InName)
+	{
+		return LoadProcesses().Any((p) => p.ProcessName.Contains(InName));
+	}
+	
+	public static bool IsPlayingMusic()
+	{
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			return false;
+		NowPlayingSessionManager npsm = new NowPlayingSessionManager();
+		return npsm.Count > 0;
+	}
 
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetForegroundWindow();
-    
-    [DllImport("user32.dll")]
-    public extern static bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+	[DllImport("user32.dll")]
+	private static extern IntPtr GetForegroundWindow();
+	
+	[DllImport("user32.dll")]
+	public extern static bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
 
-    public static IntPtr GetRandomWindow()
-    {
-        return GetForegroundWindow();
-        
-        var list = LoadProcesses();
-        while (list.Count > 0)
-        {
-            int i = Rnd.RandiRange(0, list.Count - 1);
-            var p = list[i];
-            list.RemoveAt(i);
-            if (p.MainWindowHandle == GetForegroundWindow())
-                return p.MainWindowHandle;
-        }
-    }
+	public static IntPtr GetRandomWindow()
+	{
+		return GetForegroundWindow();
+		
+		var list = LoadProcesses();
+		while (list.Count > 0)
+		{
+			int i = Rnd.RandiRange(0, list.Count - 1);
+			var p = list[i];
+			list.RemoveAt(i);
+			if (p.MainWindowHandle == GetForegroundWindow())
+				return p.MainWindowHandle;
+		}
+	}
 
-    public static Vector2 GetWindowPos(IntPtr InHandle)
-    {
-        Rect rect = new();
-        GetWindowRect(InHandle, ref rect);
-        return new (
-            rect.Left,
-            rect.Top
-        );
-    }
-    
-    public static Vector2 GetWindowSide(IntPtr InHandle, bool InLeft, float InPos)
-    {
-        Rect rect = new();
-        GetWindowRect(InHandle, ref rect);
-        return new (
-            InLeft ? rect.Left : rect.Right,
-            Mathf.Lerp(rect.Top, rect.Bottom, InPos)
-        );
-    } 
-    
-    public static Vector2 GetWindowTop(IntPtr InHandle, float InPos)
-    {
-        Rect rect = new();
-        GetWindowRect(InHandle, ref rect);
-        return new (
-            Mathf.Lerp(rect.Left, rect.Right, InPos),
-            rect.Top
-        );
-    }
+	public static Vector2 GetWindowPos(IntPtr InHandle)
+	{
+		Rect rect = new();
+		GetWindowRect(InHandle, ref rect);
+		return new (
+			rect.Left,
+			rect.Top
+		);
+	}
+	
+	public static Vector2 GetWindowSide(IntPtr InHandle, bool InLeft, float InPos)
+	{
+		Rect rect = new();
+		GetWindowRect(InHandle, ref rect);
+		return new (
+			InLeft ? rect.Left : rect.Right,
+			Mathf.Lerp(rect.Top, rect.Bottom, InPos)
+		);
+	} 
+	
+	public static Vector2 GetWindowTop(IntPtr InHandle, float InPos)
+	{
+		Rect rect = new();
+		GetWindowRect(InHandle, ref rect);
+		return new (
+			Mathf.Lerp(rect.Left, rect.Right, InPos),
+			rect.Top
+		);
+	}
 
-    public static void PushWindow(IntPtr InHandle, Vector2I InOffset)
-    {
-        Rect rect = new();
-        GetWindowRect(InHandle, ref rect);
-        SetWindowPos(InHandle, IntPtr.Zero, rect.Left + InOffset.X, rect.Top + InOffset.Y, rect.Right - rect.Left, rect.Bottom - rect.Top, 0);
-    }
+	public static void PushWindow(IntPtr InHandle, Vector2I InOffset)
+	{
+		Rect rect = new();
+		GetWindowRect(InHandle, ref rect);
+		SetWindowPos(InHandle, IntPtr.Zero, rect.Left + InOffset.X, rect.Top + InOffset.Y, rect.Right - rect.Left, rect.Bottom - rect.Top, 0);
+	}
 }
